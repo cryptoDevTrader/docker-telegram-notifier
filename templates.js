35be5f5d4e49738e4a222cfee5dab8005999f575
Anton Stolov <saopp.zori@gmail.com>
2021-01-19 14:58:08 +0200

    Update templates.js
    remove id of containers

--- templates.js
+++ templates.js
@@ -1,13 +1,13 @@
 module.exports = {
     container_start: e =>
-        `Started <b>${e.Actor.Attributes.name} (${e.Actor.Attributes.image}) {${e.Actor.ID.substr(0, 12)}}</b>`,
+        `Started <b>${e.Actor.Attributes.name} (${e.Actor.Attributes.image})</b>`,
 
     container_die: e =>
-        `Stopped <b>${e.Actor.Attributes.name} (${e.Actor.Attributes.image}) {${e.Actor.ID.substr(0, 12)}}</b>\nExit Code: <b>${e.Actor.Attributes.exitCode}</b>`,
+        `Stopped <b>${e.Actor.Attributes.name} (${e.Actor.Attributes.image})</b>\nExit Code: <b>${e.Actor.Attributes.exitCode}</b>`,
 
     'container_health_status: healthy': e =>
-        `Status <b>Healthy</b> for <b>${e.Actor.Attributes.name} (${e.Actor.Attributes.image}) {${e.Actor.ID.substr(0, 12)}}</b>`,
+        `Status <b>Healthy</b> for <b>${e.Actor.Attributes.name} (${e.Actor.Attributes.image})</b>`,
 
     'container_health_status: unhealthy': e =>
-        `Status <b>Unhealthy</b> for <b>${e.Actor.Attributes.name} (${e.Actor.Attributes.image}) {${e.Actor.ID.substr(0, 12)}}</b>`,
+        `Status <b>Unhealthy</b> for <b>${e.Actor.Attributes.name} (${e.Actor.Attributes.image})</b>`,
 };
