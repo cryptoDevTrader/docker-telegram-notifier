5ae213df8abbe00832f1292807bdecc45907e1a6
Anton Stolov <saopp.zori@gmail.com>
2021-01-20 09:50:09 +0200

    Update templates.js
    add some changes

--- templates.js
+++ templates.js
@@ -1,9 +1,11 @@
+const utils = require('./utils');
+
 module.exports = {
     container_start: e =>
-        `Started <b>${e.Actor.Attributes.name} (${e.Actor.Attributes.image})</b>`,
+        `<b>Host:</b> ${utils.getEnvVar("DOCKER_HOSTNAME")}\n<b>IP:</b> ${utils.getEnvVar("DOCKER_IP_ADDRESS")}\n<b>Container:</b>$ {e.Actor.Attributes.name}\n<b>Image:</b> ${e.Actor.Attributes.image}\n<b>Has been started</b>`,
 
     container_die: e =>
-        `Stopped <b>${e.Actor.Attributes.name} (${e.Actor.Attributes.image})</b>\nExit Code: <b>${e.Actor.Attributes.exitCode}</b>`,
+        `<b>Host:</b> ${utils.getEnvVar("DOCKER_HOSTNAME")}\n<b>IP:</b> ${utils.getEnvVar("DOCKER_IP_ADDRESS")}\n<b>Container:</b>$ {e.Actor.Attributes.name}\n<b>Image:</b> ${e.Actor.Attributes.image}\n<b>Has been stopped</b>\n<b>Exit Code:</b> ${e.Actor.Attributes.exitCode}`,
 
     'container_health_status: healthy': e =>
         `Status <b>Healthy</b> for <b>${e.Actor.Attributes.name} (${e.Actor.Attributes.image})</b>`,
