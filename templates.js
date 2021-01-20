f0097e989ac00838e2fc19bd847f47477f310ae2
Anton Stolov <saopp.zori@gmail.com>
2021-01-20 10:08:34 +0200

    Update templates.js
    some changes

--- templates.js
+++ templates.js
@@ -2,19 +2,10 @@ const utils = require('./utils');
 
 module.exports = {
     container_start: e =>
-        `<b>Host:</b> ${utils.getEnvVar("DOCKER_HOSTNAME")}\n
-<b>IP:</b> ${utils.getEnvVar("DOCKER_IP_ADDRESS")}\n
-<b>Container:</b> ${e.Actor.Attributes.name}\n
-<b>Image:</b> ${e.Actor.Attributes.image}\n
-<b>Has been started</b>`,
+        `<b>Host:</b> ${utils.getEnvVar("DOCKER_HOSTNAME")}\n<b>IP:</b> ${utils.getEnvVar("DOCKER_IP_ADDRESS")}\n<b>Container:</b> ${e.Actor.Attributes.name}\n<b>Image:</b> ${e.Actor.Attributes.image}\n<b>Has been started</b>`,
 
     container_die: e =>
-        `<b>Host:</b> ${utils.getEnvVar("DOCKER_HOSTNAME")}\n
-<b>IP:</b> ${utils.getEnvVar("DOCKER_IP_ADDRESS")}\n
-<b>Container:</b> ${e.Actor.Attributes.name}\n
-<b>Image:</b> ${e.Actor.Attributes.image}\n
-<b>Has been stopped</b>\n
-<b>Exit Code:</b> ${e.Actor.Attributes.exitCode}`,
+        `<b>Host:</b> ${utils.getEnvVar("DOCKER_HOSTNAME")}\n<b>IP:</b> ${utils.getEnvVar("DOCKER_IP_ADDRESS")}\n<b>Container:</b> ${e.Actor.Attributes.name}\n<b>Image:</b> ${e.Actor.Attributes.image}\n<b>Has been stopped</b>\n<b>Exit Code:</b> ${e.Actor.Attributes.exitCode}`,
 
     'container_health_status: healthy': e =>
         `Status <b>Healthy</b> for <b>${e.Actor.Attributes.name} (${e.Actor.Attributes.image})</b>`,
